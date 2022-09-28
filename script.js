c340faecf8ae161d78b77d33eb0e9f3b8896d12d
ErneC <mv.ernestina@gmail.com>
2022-09-27 22:02:23 -0300

    sexto exercicio JS

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Método appendChild = onde consigo colocar um elemento dentro do outro.
+
+let mensagem = document.getElementById("mensagem");
+let p = document.querySelector("#mensagem p");
+
+//mensagem.removeChild(p);
+
+//p.parentNode.removeChild(p);
+
+p.remove();
